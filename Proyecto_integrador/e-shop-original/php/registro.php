c6f708a79cb8b0dce63390c5f9ffe327fff32e63
gera-mtz <geramtz100@gmail.com>
2013-11-05 14:55:54 -0600

    otra modificacion

--- Proyecto_integrador/e-shop-original/php/registro.php
+++ Proyecto_integrador/e-shop-original/php/registro.php
@@ -7,4 +7,22 @@ $fb = $_POST("face");
 $twitter = $_POST("twitter");
 $key = $_POST("key");
 $foto = $_FILES["fotos"]["tmp_name"];
+
+function moverfoto() {
+	$prefijo = substr(md5(uniqid(rand())),0,6);
+	$destino =  "fotos/".$nombreusuario."_".$prefijo;
+		if ($tipo == "image/x-png" || $tipo == "image/gif" || $tipo == "image/jpeg") {
+			move_uploaded_file($foto,$destino);
+		}
+}
+
+function creadirectorios() {
+	$dir = "C:\Program Files\e-shop";
+	if(!mkdir($dir, 0, true))
+{
+    die('Fallo al crear carpetas...');
+}
+else
+mkdir("\foto");
+}
 ?>
