b44cee27378fea644783cc19c4ee4080001f713d
gera-mtz <geramtz100@gmail.com>
2013-11-04 20:04:00 -0600

    modificacion al programa

--- Proyecto_integrador/e-shop-original/php/registro.php
+++ Proyecto_integrador/e-shop-original/php/registro.php
@@ -1,2 +1,10 @@
 <?php
+$nombreusuario = $_POST("username");
+$contraseña = $_POST("pass");
+$recontraseña = $_POST("repass");
+$correo = $_POST("email");
+$fb = $_POST("face");
+$twitter = $_POST("twitter");
+$key = $_POST("key");
+$foto = $_FILES["fotos"]["tmp_name"];
 ?>
