db7bccc640411f251aab3b05ef94a1baf25529c5
gera-mtz <geramtz100@gmail.com>
2013-11-07 20:23:32 -0600

    terminando todas las funciones de php

--- Proyecto_integrador/e-shop-original/index.php
+++ Proyecto_integrador/e-shop-original/index.php
@@ -1,45 +1,42 @@
 <!--webpage hecho por jose gerardo martinez moran... fines educativos-->
-
 <!doctype html>
 <!--para html5-->
 <html>
-
-<head>
-	<!--cabeza de la pagina-->
-	<title>e-shop :: Tu tienda electronica</title>
-	<meta charset="utf-8"/>
-	<!--codigo que se usara para la pagina, aplicada a todas-->
-	<link name rel="stylesheet" type="text/css" href="css/index.css" />
-	<!--link que se redirige a donde se encuentra el archivo de estilo-->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" >
-	<!--para el uso de responsive design-->
-</head>
-
-<body>
-	<div id="nav">
-		<div id="right">
+	<head>
+		<!--cabeza de la pagina-->
+		<title>e-shop :: Tu tienda electronica</title>
+		<meta charset="utf-8"/>
+		<!--codigo que se usara para la pagina, aplicada a todas-->
+		<link name rel="stylesheet" type="text/css" href="css/index.css" />
+		<!--link que se redirige a donde se encuentra el archivo de estilo-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" >
+		<!--para el uso de responsive design-->
+	</head>
+	<body>
+		<div id="nav">
+			<div id="right">
 			<img src="img/cart.fw.png"></div>
-		<div id="left">
-			<h3 id="slogan" align="right">Tu tienda de electronica</h3>
+			<div id="left">
+				<h3 id="slogan" align="right">Tu tienda de electronica</h3>
+			</div>
 		</div>
-	</div>
-	<div id="container" >
-		<article class="advise" id="advise">
+		<div id="container" >
+			<article class="advise" id="advise">
 			<?
-	$dir = "C:\Program Files\e-shop";
-	$csvadmin = "admin.csv";
-    if (file_exists($dir)) {
-		if ($exist = opendir($dir)) {
+			$dir = "C:\\e-shop";
+			$csvadmin = "admin.csv";
+			if (file_exists($dir)) {
+			if ($exist = opendir($dir)) {
 			while (($dir = readdir($exist)) !== false) {
-				if (file_exists($csvadmin)) {
-	}
-	else {
-		?>
+			if (file_exists($csvadmin)) {
+			}
+			else {
+			?>
 			<!--Uso del modal dialog-->
 			<div id="noinstalado">
 				<!--aqui se usara el link que comunicara el diseño de la ventana modal-->
 				<a href="#openModal">
-					¡¡¡Esta pagina no esta instalado en su computadora!!!, le pedimos que haga click aqui para su correcta instalacion
+				¡¡¡Esta pagina no esta instalado en su computadora!!!, le pedimos que haga click aqui para su correcta instalacion
 				</a>
 				<!--link para abrir el modalDialog-->
 				<div id="openModal" class="modalDialog">
@@ -51,46 +48,58 @@
 						<h2>Bienvenido!!!</h2>
 						<p>A continuacion se solicitara algunos datos para su registro</p>
 						<form name="registro" action="php/registro.php" method="post" enctype="multipart/form-data">
-							<input id="username" type="text" name="username" placeholder="Nombre de usuario" />
+							<input id="username" type="text" name="username" placeholder="Nombre de usuario*" />
 							<br />
-							<input id="pass" type="password" name="pass" placeholder="Contraseña" />
+							<input id="pass" type="password" name="pass" placeholder="Contraseña*" />
 							<br />
-							<input id="repass" type="password" name="repass" placeholder="Repite Contraseña" />
+							<input id="repass" type="password" name="repass" placeholder="Repite Contraseña*" />
 							<br />
-							<input id="email" type="email" name="email" placeholder="e-mail" />
+							<input id="email" type="email" name="email" placeholder="e-mail*" />
 							<br />
 							<input id="face" type="text" name="face" placeholder="Facebook" />
 							<br />
 							<input id="twitter" type="text" name="twitter" placeholder="Twitter" />
+							<br />														
+							<input id="key" type="text" name="key" placeholder="Clave de instalacion*" />
+							<!--uso de scrpts para añadir los guiones automaticos de la clave.. llamo las librerias-->
+							<script src="js/jquery-2.0.3.js" type="text/javascript"></script>
+							<script src="js/jquery.maskedinput.js" type="text/javascript"></script>
+							<script type="text/javascript">
+							<!--la funcion jquery-->
+							jQuery(function($)
+							{
+								$("#key").mask("a99a-9999-99a9-a990",{placeholder:" "});
+							});
+							</script>
 							<br />
-							<input id="key" type="text" name="key" placeholder="Clave de instalacion" />
-							<br />
+							<input type="label" placeholder="Foto*" />
 							<input type="file" id="fotos" name="fotos[]" multiple  placeholder="Inserte una foto aqui" accept="image/jpeg,image/gif,image/png"/>
 							<output id="pre"></output>
 							<script language="javascript" type="text/javascript" src="js/previsualizacion.js"></script>
 							<br />
+							<h4>* = Datos obligatorios</h4>
 							<input type="button" name="enviar" id="enviar" placeholder="Instalar" />
 						</form>
 					</div>
 				</div>
 			</div>
 			<?
-	}
-	}
-	}
-	}
-	?>
+			}
+			}
+			}
+			}
+			?>
 			<article class="login" id="login">
-				<form id="log" name="log" method="post">
-					<input name="user" type="text" id="user" placeholder="Nombre de usuario"/>
-					<br />
-					<input name="pss" type="password" id="pss" placeholder="Contraseña" />
-					<br />
-					<input type="submit" value="Iniciar" id="start" name="start" />
-				</form>
+			<form id="log" name="log" method="post">
+				<input name="user" type="text" id="user" placeholder="Nombre de usuario"/>
+				<br />
+				<input name="pss" type="password" id="pss" placeholder="Contraseña" />
+				<br />
+				<input type="submit" value="Iniciar" id="start" name="start" />
+			</form>
 			</article>
 		</div>
-	</article>
-<footer>Este solo es una prueba</footer>
+		</article>
+	<footer>Este solo es una prueba</footer>
 </body>
 </html>
--- Proyecto_integrador/e-shop-original/php/registro.php
+++ Proyecto_integrador/e-shop-original/php/registro.php
@@ -7,22 +7,33 @@ $fb = $_POST("face");
 $twitter = $_POST("twitter");
 $key = $_POST("key");
 $foto = $_FILES["fotos"]["tmp_name"];
+$destino;
+$linea;
 
-function moverfoto() {
+function moverfoto($destino) {
 	$prefijo = substr(md5(uniqid(rand())),0,6);
-	$destino =  "fotos/".$nombreusuario."_".$prefijo;
+	$destino =  "fotos\\".$nombreusuario."_".$prefijo;
 		if ($tipo == "image/x-png" || $tipo == "image/gif" || $tipo == "image/jpeg") {
 			move_uploaded_file($foto,$destino);
+			return $destino;
 		}
 }
 
-function creadirectorios() {
-	$dir = "C:\Program Files\e-shop";
+function creadirectorios($dir) {
+	$dir = "C:\\e-shop";
 	if(!mkdir($dir, 0, true))
-{
-    die('Fallo al crear carpetas...');
+	    die('Fallo al crear carpetas...');
+	else
+		mkdir("\\fotos");
+	return $dir;
 }
-else
-mkdir("\foto");
+
+$csv = fopen("C:\\e-shop\\admin.csv", "x");
+if (isset($nombreusuario) && !empty($nombreusuario) && isset($contraseña) && !empty($contraseña) && isset($recontraseña) && !empty($recontraseña) && isset($correo) && !empty($correo) && isset($foto) && !empty($foto) &&)
+	if ($contraseña == $recontraseña) {
+		if ($key == "X24A-4922-99O1-X430") {
+
+		}
+	}
 }
 ?>
